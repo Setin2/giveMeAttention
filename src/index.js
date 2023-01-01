994062159ae1ced2003c4f1f5a303d4bf975fc6c
Remus Stefan Cernat <stefancernat88@yahoo.com>
2023-01-01 15:54:52 +0100

    finished cssing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-console.log("working :)");
+var popout = document.createElement("div");
+popout.setAttribute("class", "popout");
+document.body.appendChild(popout);
+
+var btn = document.createElement("div");
+btn.setAttribute("class", "btn");
+popout.appendChild(btn);
+
+var heart = document.createElement("img");
+heart.src = chrome.runtime.getURL("images/heart.png");
+heart.setAttribute("width", "60");
+btn.appendChild(heart);
+
+btn.addEventListener('click', function() {
+    console.log("heart");
+});
+
+$(document).ready(function(){
+    animateDiv();
+    
+});
+
+function makeNewPosition(){
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 70;
+    var w = $(window).width() - 70;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];        
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+    var oldq = $(btn).offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $(btn).animate({ top: newq[0], left: newq[1] }, speed, function(){
+      animateDiv();        
+    });
+};
+
+function calcSpeed(prev, next) {
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    
+    var greatest = x > y ? x : y;
+    var speedModifier = 0.1;
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+}
