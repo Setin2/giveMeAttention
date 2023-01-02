a87d2f7a6bf65b85d8ae766bc41bf250aa62810d
Remus Stefan Cernat <stefancernat88@yahoo.com>
2023-01-02 15:26:07 +0100

    submit button changed

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,12 +1,33 @@
-document.getElementById("save").addEventListener("click", function(){
+/*document.getElementById("save").addEventListener("click", saveSettings);*/
+
+function saveSettings(){
     var subject = document.getElementById("subject").value;
     var body = document.getElementById("body").value;
 
-    saveSettings("iwantattention", subject + ";" + body);
-});
-
-function saveSettings(key, value){
-    chrome.storage.sync.set({ "iwantattention": value }, function(){
-        alert("settings saved");
+    chrome.storage.sync.set({ "iwantattention": subject + ";" + body }, function(){
+        //alert("settings saved");
     });
 }
+
+$('input[role=submit]').click(function () {
+    var $t = $('input[role=submit]');
+    var hasClass = $t.hasClass('done');
+    if (!hasClass){
+        $t.addClass('clicked');
+        setTimeout(function(){
+            $t.removeClass('clicked').addClass('done').attr({
+                value: 'Saved',
+            });
+        }, time()); 
+    } else if (hasClass){
+        $t.removeClass().attr({
+            value: 'Submit',
+        });
+    }
+
+    saveSettings();
+  });
+  
+  function time() {
+    return 1000 + Math.random() * 2000
+  }
